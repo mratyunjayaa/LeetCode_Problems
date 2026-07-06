cd58d32d13eb737975c85b1d538be46784b4e1bb
Vishal Tomar <tovishal261@gmail.com>
2026-07-06 12:09:18 +0530

    Time: 4 ms (2.85%) | Memory: 16.4 MB (10.92%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(head == NULL || head->next == NULL) return head; 
-        
-        vector<int> ans; 
-        vector<int> org;
-        
-        // Copy linked list values into vector
-        ListNode* dummy = head; 
-        while(dummy){
-            ans.push_back(dummy->val);
-            dummy = dummy->next; 
+
+        // OptimL SOLUTION 
+        if (head == nullptr)
+            return head;
+
+        ListNode* curr = head;
+        ListNode* newhead = curr;
+        while (curr != nullptr && curr->next != nullptr) {
+            if (curr->val == curr->next->val) {
+                ListNode* temp = curr->next;
+                curr->next = temp->next;
+                delete temp;
+            } else
+                curr = curr->next;
         }
-        
-        // Two-pointer logic to collect unique values
-        int slow = 1, fast = 1; 
-        org.push_back(ans[0]);
-        
-        for(int i = 1; i < ans.size(); i++){
-            if(ans[i] != ans[i-1]) {
-                org.push_back(ans[i]);
-            }
-        }
-        
-        // Build new linked list from org
-        ListNode* newhead = new ListNode(org[0]);
-        ListNode* current = newhead; 
-        for(int i = 1; i < org.size(); i++){
-            current->next = new ListNode(org[i]);
-            current = current->next; 
-        }
-        
-        return newhead; 
+        return newhead;
     }
 };
