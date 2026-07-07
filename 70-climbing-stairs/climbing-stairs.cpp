78ff14bfd0c1b099a3c0dc559b50b6f82598cef8
Vishal Tomar <tovishal261@gmail.com>
2026-07-07 15:06:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (36.67%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    int count(int i, int n, vector<int>& dp) {
+        if (i == n)
+            return 1;
+        if (i > n)
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+        return dp[i] = count(i + 1, n, dp) + count(i + 2, n, dp);
+    }
+
+public:
+// Top Down Methodd 
+    int climbStairs(int n) {
+
+        vector<int> dp(n + 2, -1);
+        return count(0, n, dp);
+    }
+};
