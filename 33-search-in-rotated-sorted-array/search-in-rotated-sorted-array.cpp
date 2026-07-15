693b30850db053de33e1c65d3096702ef3f6987c
Vishal Tomar <tovishal261@gmail.com>
2026-07-15 12:26:32 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (96.51%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // Brute Force
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target)
-                return i;
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[low] <= nums[mid]) {
+
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else
+                    low = mid + 1;
+            }else {
+
+                if (target > nums[mid] && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
         }
-        return -1 ;
+        return -1;
     }
 };
