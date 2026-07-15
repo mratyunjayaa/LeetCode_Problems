42d201c3f715863dfd36eed1ecec9805f2359085
Vishal Tomar <tovishal261@gmail.com>
2026-07-15 13:27:47 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (45.23%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,12 +1,31 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        //Brute FOrce
-        for(int i = 0  ; i < nums.size() ; i++){
-            if(nums[i]==target){
-                return true ;
+        // Optimal BS Solution 
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return true;
+            if (nums[mid] == nums[low] && nums[mid] == nums[high]) {
+                low++, high--;
+                continue;
+            }
+            if (nums[low] <= nums[mid]) {
+
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else
+                    low = mid + 1;
+            } else {
+
+                if (target > nums[mid] && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
             }
         }
-        return false ;
+        return false;
     }
 };
