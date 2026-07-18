bbcfd2971a5c211fa09ba0fa61718b37e8fc09e1
Vishal Tomar <tovishal261@gmail.com>
2026-07-18 13:11:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (50.12%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        // Optiamal Binary Search 
-        if(x==0) return 0 ;
-        long long ans = 1 ; 
-        int low = 0 , high = x ;
-        while(low<=high){
-            long long mid = low + (high - low ) / 2 ;
-            long long val = mid * mid ;
-            if(val <= x){
-                low = mid + 1 ;
-            }
-            else{
-                high = mid -1 ;
-            }
+        int low = 0, high = x;
+
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+            long long val = mid * mid;
+            if (val == x)
+                return mid;
+            else if (val < x)
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
-        return high  ;
+
+        return high;
     }
 };
