55db85851d0bff3e0f4d6cb7dfb86d1d3965277d
Vishal Tomar <tovishal261@gmail.com>
2026-07-18 12:39:30 +0530

    Time: 23 ms (6.29%) | Memory: 8.5 MB (50.12%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        // brute Force 
+        if(x==0) return 0 ;
+        long long ans = 1 ; 
+        for(long long  i = 1 ; i <= x ; i++){
+            if(i*i <= x){
+                ans = i ;
+            }
+            else{
+                break ;
+            }
+        }
+        return ans;
     }
 };
