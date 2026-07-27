88b789498bdb37fa73f468a65a1ba8484802dc8f
Vishal Tomar <tovishal261@gmail.com>
2026-07-27 09:27:32 +0530

    Time: 0 ms (100.00%) | Memory: 31.1 MB (67.86%) - LeetSync

--- 4354-unique-middle-element/unique-middle-element.cpp
+++ 4354-unique-middle-element/unique-middle-element.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     bool isMiddleElementUnique(vector<int>& nums) {
         if(nums.size() < 2) return true ;
-        int low = 0 , high = nums.size()-1 , count = 0 ;
-        int mid = (low + high) / 2 ;
+        int count = 0 ,  mid = nums.size() / 2 ;
         for(auto x : nums) {
             if(nums[mid]==x){
                 count++;
