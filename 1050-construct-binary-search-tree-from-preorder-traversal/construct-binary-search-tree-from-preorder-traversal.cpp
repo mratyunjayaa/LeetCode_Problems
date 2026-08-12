57d912a0a5b6398b2faa609481346d94695b3f84
Vishal Tomar <tovishal261@gmail.com>
2026-08-12 15:42:31 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (46.88%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -10,18 +10,35 @@
  * };
  */
 class Solution {
-       TreeNode * build(vector<int>& preorder , int &i , int bound){
-        if(preorder.size()==i )  return NULL ;
-        if(preorder[i] > bound) return NULL ;
-
-        TreeNode * root = new TreeNode (preorder[i++]);
-        root->left = build(preorder , i , root->val) ;
-        root->right = build(preorder , i , bound) ;
-        return root ;
-       }
 public:
+
+    TreeNode* solve(vector<int>& preorder, int& i,
+                long long low, long long high) {
+
+        if (i == preorder.size())
+            return NULL;
+
+        // Current value is not allowed here
+        if (preorder[i] <= low || preorder[i] >= high)
+            return NULL;
+
+        TreeNode* root = new TreeNode(preorder[i]);
+        i++;
+
+        // Construct left subtree
+        root->left = solve(preorder, i, low, root->val);
+
+        // Construct right subtree
+        root->right = solve(preorder, i, root->val, high);
+
+        return root;
+    }
+
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        int i = 0 ;
-        return build(preorder , i , INT_MAX);
+
+        int i = 0;
+
+        return solve(preorder, i,
+                     LLONG_MIN, LLONG_MAX);
     }
 };
