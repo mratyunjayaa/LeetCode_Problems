bc033f0ab4cf26137e2e7b7049ce2030dba21597
Vishal Tomar <tovishal261@gmail.com>
2026-08-22 11:19:54 +0530

    Time: 8 ms (49.76%) | Memory: 7.6 MB (88.86%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int fib(int n) {
-        int t1 = 0 , t2 = 1 ;
-        if(n<= 1) return n ;
-        int res = 0 ;
-        for(int j = 2 ; j <= n ; j++){
-             res = t1+t2;
-             t1 = t2;
-             t2 = res;
-        }
-        return res;
+        return n <= 1 ? n : fib(n-1) + fib(n-2); 
     }
 };
