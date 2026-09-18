982304a11e5668f38005453a4b1421162fd50c26
Vishal Tomar <tovishal261@gmail.com>
2026-09-18 17:01:32 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (73.27%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,19 +1,47 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        if(nums.empty()) return {};
-        unordered_map<int , int> mp;
+        if(nums.size()<=1) return nums;
+        int candicate1 = 0 ;
+        int candicate2 = 0 ;
+        int count1 = 0 ;
+        int count2 = 0 ;
+
         for(auto it : nums){
-            mp[it]++;
-        }
-        int size = nums.size();
-        int maj = size / 3;
-       vector<int>Ans;
-        for(auto it : mp){
-            if(it.second >  maj){
-                Ans.push_back(it.first);
+            if(it == candicate1 ){
+                count1++;
+            }
+            else if(it == candicate2 ){
+               count2++;
+            }
+            else if(count1==0){
+                candicate1 = it;
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                candicate2 = it;
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
             }
         }
-        return Ans;
+
+        int freq = 0 ;
+        int freq2 = 0 ;
+        for(auto it : nums){
+            if(it == candicate1) freq++;
+            if(it == candicate2) freq2++;
+        }
+        vector<int>ans;
+
+        if (freq> nums.size() / 3)
+            ans.push_back(candicate1);
+
+        if (freq2 > nums.size() /3 && candicate1 != candicate2)
+            ans.push_back(candicate2);
+
+         return ans;       
     }
 };
